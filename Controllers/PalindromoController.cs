cbe8cb8fca3f39c0f7cab26640dc006640b742eb
lordivanba <ivanbamail@gmail.com>
2021-09-27 22:41:11 -0500

    Add project files.

---
+++ Controllers/PalindromoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actividad2_Palindromo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PalindromoController : ControllerBase
+    {
+        [HttpGet("{palabra}")]
+        public string Get(string Palabra) 
+        {
+            string palabra, inverso, caracter, resultado;
+            int i;
+            int count = Palabra.Split(' ').Length;
+
+            palabra = Palabra.ToLower().Replace(" ","");
+            i = palabra.Length;
+            inverso = "";
+
+            for (int x=i-1; x>=0; x--)
+            {
+                caracter = palabra.Substring(x, 1);
+                inverso = inverso + caracter;
+            }
+
+            if (palabra == inverso)
+            {
+                resultado = $"La palabra: {Palabra}, es palindromo";
+            }
+            else 
+            {
+                resultado = $"La palabra: {Palabra}, no es palindromo";
+            }
+
+            return $"{resultado} Total de palabras: {count}";
+        }
+    }
+}
---
+++ Palindromo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actividad2_Palindromo
+{
+    public class Palindromo
+    {
+        public string mensaje { get; set; }
+        public int i { get; set; }
+    }
+}
